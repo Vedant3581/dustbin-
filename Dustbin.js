f73fdfd8d9a54de217a30f2ae38f704dd15a1245
Vedant3581 <66295277+Vedant3581@users.noreply.github.com>
2020-06-13 16:33:53 +0530

    Add files via upload

--- Dustbin.js
+++ Dustbin.js
@@ -1,4 +1,4 @@
-class Paper {
+class Dustbin {
     constructor(x, y, width, height) {
         var options = {
             isStatic: false,
@@ -6,22 +6,16 @@ class Paper {
             friction: 0.5,
             density: 1.2
         }
-        this.body = Bodies.circle(x, y, 55, 55,options);
-        this.width = 55;
-        this.height = 55;
-
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        
         World.add(world, this.body);
     }
     display() {
         var pos = this.body.position;
         var angle = this.body.position;
-
-        Push();
-        translate(pos.x, pos.y);
-        rotate(angle);
-        strokeWeight=3
-        fill=("blue");
-        rectMode(CENTER);
-        rect(0,0,this.width,this.height);
+        rectMode(CENTER)
+        dustbin.display();
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,58 +1,58 @@
-var paper;
-var dustbin,dustbin2,dustbin3,ground;
 
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-function preload()
-{
-	
+var paper;
+var dustbin, dustbin2, dustbin3, ground;
+
+function preload() {
+
 }
 
 function setup() {
-	createCanvas(800, 700);
-
-	dustbinSprite=createSprite(600,650,200,20)
-	dustbinSprite.shapeColor="black"
-	
-	dustbin1Sprite=createSprite(700,600,20,100)
-	dustbin1Sprite.shapeColor="black"
-
-	dustbin2Sprite=createSprite(500,600,20,100)
-	dustbin2Sprite.shapeColor="black"
-
+	var canvas=createCanvas(800, 700);
 	engine = Engine.create();
 	world = engine.world;
 
-	paperBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1.5, isStatic:false});
+
+	dustbinSprite = createSprite(600, 650, 200, 20)
+	dustbinSprite.shapeColor = "black"
+
+	dustbin1Sprite = createSprite(700, 600, 20, 100)
+	dustbin1Sprite.shapeColor = "black"
+
+	dustbin2Sprite = createSprite(500, 600, 20, 100)
+	dustbin2Sprite.shapeColor = "black"
+
+
+	paperBody = Bodies.circle(width / 2, 200, 5, { restitution: 1.5, isStatic: false });
 	World.add(world, paperBody);
-    ellipse(100,600, 55, 55);
+	ellipse(100,600, 55, 55);
+
+	ground = Bodies.rectangle(width / 2, 650, width, 10, { isStatic: true });
+	World.add(world, ground);
 
 
-    ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
-
-
-	Engine.run(engine);
+	
 }
-
-
-
 function draw() {
-  rectMode(CENTER);
-  background("white")
-  drawSprites();
- 
+	rectMode(CENTER);
+	background("white");
+	Engine.update(engine);
+	
+
+	drawSprites();
 }
+
 
 function keyPressed() {
-	if (keyCode === UP_ARROW) {
-		Matter.Body.applyForce(paperBody, position, force)
-	 }
-   }
-   
+	if (keycode === UP_ARROW) {
+		Matter.Body.applyForce(paperBody, position, force,{x : 85,y:-85});
+	}
+}
+
 
 
 
